2e8ac36a06bb04f0f35e14cd2fad9d7fcd3ff5fa
gmogithub <mouyal.gregory@gmail.com>
2023-12-05 14:22:26 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ display: "flex" }}>
+        <div style={{
+          border: "1px solid red",
+          height: 56,
+          width: 180,
+          minWidth: 180,
+          maxWidth: 180,
+        }}></div>
+        <div style={{
+          border: "1px solid red",
+          height: 56,
+          width: 100,
+          minWidth: 100,
+          maxWidth: 100,
+        }}></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div style={{ display: "flex" }}>
+        <div style={{
+          border: "1px solid blue",
+          minWidth: 180,
+          maxWidth: 180,
+          minHeight: 52,
+          maxHeight: 52,
+        }}></div>
+        <div style={{
+          border: "1px solid blue",
+          minWidth: 100,
+          maxWidth: 100,
+          minHeight: 52,
+          maxHeight: 52,
+        }}></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
